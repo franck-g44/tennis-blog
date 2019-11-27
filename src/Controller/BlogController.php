f1c9abd1eae3cb1ec63413dc859af5ff26b89eba
franck-g44 <franck44250@gmail.com>
2019-11-27 13:10:14 +0100

    interpolation

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -4,8 +4,11 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 use App\Entity\Article;
 use App\Repository\ArticleRepository;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class BlogController extends AbstractController
 {
@@ -25,16 +28,48 @@ class BlogController extends AbstractController
      * @Route("/", name="home")
      */
     public function home() {
+
         return $this->render('blog/home.html.twig');
     }
+
+    /**
+     * @Route("/blog/new", name="blog_create")
+     */
+    public function create(Request $request) {
+        $article = new Article();
+
+        $form = $this->createFormBuilder($article)
+                     ->add('title', TextType::class, [
+                         'attr' => [
+                             'placeholder' =>"Titre de l'article"
+                         ]
+                     ])
+                     ->add('content', TextareaType::class, [
+                        'attr' => [
+                            'placeholder' =>"Contenu de l'article"
+                        ]
+                    ] )
+                     ->add('image', TextType::class, [
+                        'attr' => [
+                            'placeholder' =>"Image de l'article"
+                        ]
+                    ])
+                     ->getForm();
+
+        return $this->render('blog/create.html.twig', [
+            'formArticle' => $form->createView()
+        ]);
+    }
+
     /**
      * @Route("/blog/{id}", name="blog_show")
      */
     public function show(Article $article){
         
-
         return $this->render('blog/show.html.twig', [
             'article' => $article
         ]);
     }
+
+    
 }
